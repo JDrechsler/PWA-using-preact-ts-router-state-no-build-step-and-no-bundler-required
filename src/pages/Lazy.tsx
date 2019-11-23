c4c4429135db9b293f50a6ad6c3a3c28934abe13
JDrechsler <johannes.drechsler@arvatosystems.com>
2019-11-23 11:00:36 -0500

    Changed formatting

--- src/pages/Lazy.tsx
+++ src/pages/Lazy.tsx
@@ -7,6 +7,11 @@ class LazyPage extends Component {
     console.log('Click');
     customStore.btnClicked += 1;
   }
+
+  btnClickSubtract() {
+    console.log('Click -');
+    customStore.btnClicked -= 1;
+  }
   render() {
     const myStyle = {
       color: 'chocolate',
@@ -43,18 +48,13 @@ class LazyPage extends Component {
         <h2 style={myStyle} class="hmmm">
           Lazy
         </h2>
-        <p style={myCSS('cornflowerblue').test}>
-          Your favorite food is: {customStore.favFood}
-        </p>
-        <p
-          style={
-            customStore.btnClicked < 5 ? myCSS('black').test : myCSS('red').test
-          }
-        >
-          Button clicked: {customStore.btnClicked}
-        </p>
+        <p style={myCSS('cornflowerblue').test}>Your favorite food is: {customStore.favFood}</p>
+        <p style={customStore.btnClicked < 5 ? myCSS('black').test : myCSS('red').test}>Button clicked: {customStore.btnClicked}</p>
         <button style={myStyles.redishBackground} onClick={this.btnClick}>
           Click me
+        </button>
+        <button style={myStyles.redishBackground} onClick={this.btnClickSubtract}>
+          Click me -
         </button>
       </section>
     );
